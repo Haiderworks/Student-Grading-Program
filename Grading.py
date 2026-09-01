2099757c77da8de786acb8108381a43860bbc15d
Haider ijaz <haider.ijaz1783@gmail.com>
2026-09-01 20:10:24 +0500

    Modify grading output format
    Updated grading output to include 'Grade:' prefix for each letter grade.

--- Grading.py
+++ Grading.py
@@ -2,14 +2,14 @@ score = int(input("Enter your score: "))
 if score > 100:
     print("Invalid score")
 elif score >= 90 and score < 100:
-    print("A")
+    print("Grade:A")
 elif score >= 80 and score < 90:
-    print("B")
+    print("Grade:B")
 elif score >= 70 and score < 80:
-    print("C")
+    print("Grade:C")
 elif score >= 60 and score < 70:
-    print("D")
+    print("Grade:D")
 else:
-    print("F")
+    print("Grade:F")
 
 
